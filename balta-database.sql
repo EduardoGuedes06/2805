8298a930ab4a07c6c4080641ec84711911b221b4
Eduardo Guedes Pereira Silva <90363934+EduardoGuedes06@users.noreply.github.com>
2022-08-11 11:48:52 -0300

    Update balta-database.sql

--- balta-database.sql
+++ balta-database.sql
@@ -89,6 +89,7 @@ CREATE TABLE [CareerItem]
     [Description] TEXT NOT NULL,
     [Order] TINYINT NOT NULL,
     
+    
     CONSTRAINT [PK_CareerItem] PRIMARY KEY ([CourseId], [CareerId]),
     CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN KEY ([CareerId]) REFERENCES [Career] ([Id]),
     CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id])
